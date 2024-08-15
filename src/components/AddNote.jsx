198e35fca443a2b83a1f1c3cbafb75117ff14fb0
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2024-08-14 21:50:29 -0300

    Third Commit
    Third Commit add notes

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,33 +1,64 @@
+// Hooks
+import { useState } from "react";
+
+// Icons
 import { BiPlus, BiPin, BiX, BiSolidFilePlus } from "react-icons/bi";
 
 const AddNote = () => {
+  const [notes, setNotes] = useState([]);
+  const [text, setText] = useState("");
+  const [id, setId] = useState(0);
+
+  const addNote = () => {
+    if (text === "") {
+      alert("Adicione o que deseja anotar");
+      return;
+    }
+
+    const noteObject = {
+      id: generateId(),
+      content: text,
+      fixed: false,
+    };
+
+    setNotes([...notes, noteObject]);
+
+    setText("");
+  };
+
+  const generateId = () => {
+    setId(Math.floor(Math.random() * 5000));
+    return id;
+  };
+
   return (
     <div>
       <div className="add-note-container">
-        <input type="text" placeholder="O que deseja anotar?" />
-        <button className="add-note">
+        <input
+          type="text"
+          value={text}
+          placeholder="O que deseja anotar?"
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button onClick={addNote} className="add-note">
           <BiPlus />
         </button>
       </div>
       <div className="notes-container">
-        <div className="note">
-          <textarea placeholder="Adicione algum texto"></textarea>
-          <BiPin className="pin" />
-          <BiX className="x" />
-          <BiSolidFilePlus className="plus" />
-        </div>
-        <div className="note">
-          <textarea placeholder="Adicione algum texto"></textarea>
-          <BiPin className="pin" />
-          <BiX className="x" />
-          <BiSolidFilePlus className="plus" />
-        </div>
-        <div className="note fixed">
-          <textarea placeholder="Adicione algum texto"></textarea>
-          <BiPin className="pin" />
-          <BiX className="x" />
-          <BiSolidFilePlus className="plus" />
-        </div>
+        {notes.length > 0 ? (
+          <>
+            {notes.map((note) => (
+              <div className="note" key={note.id}>
+                <textarea>{note.content}</textarea>
+                <BiPin className="pin" />
+                <BiX className="x" />
+                <BiSolidFilePlus className="plus" />
+              </div>
+            ))}
+          </>
+        ) : (
+          <p>Você ainda não tem notas...</p>
+        )}
       </div>
     </div>
   );
