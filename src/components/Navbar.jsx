15082cc7bbde421ca1f239591b59a1faf2597867
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2024-08-20 21:39:40 -0300

    Last Commit
    Last Commit adicionado a função de exportar notas e responsividade do projeto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,21 @@
 // Icons
 import { BiSearch, BiDownload } from "react-icons/bi";
 
+// Axios
+import axios from "axios";
+
 // Navigation
 import { Link, useNavigate } from "react-router-dom";
 
 // Hooks
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 // Message
 import { toast } from "react-toastify";
 
 const Navbar = () => {
   const [search, setSearch] = useState("");
+  const [notes, setNotes] = useState([]);
   const navigate = useNavigate();
   const inputRef = useRef("");
 
@@ -24,6 +28,41 @@ const Navbar = () => {
       navigate("/search", { state: { search } });
       inputRef.current.value = "";
     }
+  };
+
+  const exportNotes = async () => {
+    try {
+      const response = await axios.get("http://localhost:8800/notes");
+      setNotes(response.data.notes);
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (notes.length > 0) {
+      exportData();
+    }
+  }, [notes]);
+
+  const exportData = () => {
+    const notas = notes;
+    const csvString = [
+      ["ID", "Conteúdo", "Fixado?"],
+      ...notas.map((note) => [note._id, note.content, note.fixed]),
+    ]
+      .map((e) => e.join(","))
+      .join("\n");
+
+    const element = document.createElement("a");
+
+    element.href = "data:text/csv;charset=utf-8," + encodeURI(csvString);
+
+    element.target = "_blank";
+
+    element.download = "notes.csv";
+
+    element.click();
   };
 
   return (
@@ -47,7 +86,7 @@ const Navbar = () => {
           </div>
         </li>
         <li>
-          <button className="export-notes">
+          <button className="export-notes" onClick={exportNotes}>
             Exportar CSV <BiDownload />{" "}
           </button>
         </li>
